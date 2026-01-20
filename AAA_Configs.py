1268c56892097e4c46965caf9ffbc94682eb875d
Dominic Welsh <domwelsh@gmail.com>
2026-01-19 21:36:32 -0800

    Updated AAA_Configs.py for 6 channels. Might need to adjust normalization values, original ones saved in comments

--- AAA_Configs.py
+++ AAA_Configs.py
@@ -1,8 +1,10 @@
-normalization_mean = (0.485, 0.456, 0.406, 0.485, 0.456, 0.406, 0.485, 0.456, 0.406, 0.485)
-normalization_std = (0.229, 0.224, 0.225, 0.229, 0.224, 0.225, 0.229, 0.224, 0.225, 0.229)
+normalization_mean = (0.485, 0.456, 0.406, 0.485, 0.456, 0.406)
+# (0.485, 0.456, 0.406, 0.485, 0.456, 0.406, 0.485, 0.456, 0.406, 0.485)
+normalization_std = (0.229, 0.224, 0.225, 0.229, 0.224, 0.225)
+# (0.229, 0.224, 0.225, 0.229, 0.224, 0.225, 0.229, 0.224, 0.225, 0.229)
 
-channel_nums = 10
-selected_nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+channel_nums = 6
+selected_nums = [0, 1, 2, 3, 4, 5]
 # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
@@ -20,7 +22,7 @@ Train_weight_path = "G:/BACDM/logs/" # 权重保存的路径
 Train_pretrained_path = None
 
 
-Test_im_pathA = "G:/BACDM/data/before/"
-Test_im_pathB = "G:/BACDM/data/after/"
-Test_det_path = "G:/BACDM/pre/"
-Test_weight_path = "G:/BACDM/logs/B12118A8765432.pth"
+Test_im_pathA = r"C:\Users\domwe\Documents\thesis_work\BACDM\test_data\before"
+Test_im_pathB = r"C:\Users\domwe\Documents\thesis_work\BACDM\test_data\after"
+Test_det_path = r"C:\Users\domwe\Documents\thesis_work\BACDM\test_data\predictions"
+Test_weight_path = r"C:\Users\domwe\Documents\thesis_work\logs\B12118A432.pth"
